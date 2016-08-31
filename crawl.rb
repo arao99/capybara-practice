db05686c77c8a3d94fe0d8ca89e6cd73c515b895
arao <yusuque.swallows@gmail.com>
2016-08-31 14:38:23 +0900

    動作確認の下準備

---
+++ crawl.rb
@@ -0,0 +1,29 @@
+require 'capybara/poltergeist'
+require 'pp'
+
+# Capybaraインスタンスの作成
+def generate_crawler
+  options = {:js_errors => false, :timeout => 1000, phantomjs_logger: Logger.new('/dev/null')}
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, options)
+  end
+  
+  session = Capybara::Session.new(:poltergeist)
+  
+  session.driver.headers = {
+    'User-Agent' => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+  }
+  
+  session
+end
+
+### 以下クロール処理
+
+# Capybaraインスタンス作成
+session = generate_crawler
+
+# トップページ遷移
+session.visit 'https://arao99.github.io/capybara-practice/'
+
+# 動作確認
+puts session.html
